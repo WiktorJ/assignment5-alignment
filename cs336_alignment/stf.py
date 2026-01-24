3dab0d32ca6f27381a2f9ce36e5d0c3878ead157
wiktorj <wiktor.jurasz@gmail.com>
2026-01-24 16:36:26 +0100

    Added entropy and res logits to sft

--- cs336_alignment/stf.py
+++ cs336_alignment/stf.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn.functional as F
+from transformers import PreTrainedModel
 
 
 def tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer):
@@ -39,3 +41,42 @@ def tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer):
         "labels": labels,
         "response_mask": reponse_mask,
     }
+
+
+def compute_entropy(logits: torch.Tensor) -> torch.Tensor:
+    log_probs = F.log_softmax(logits, dim=-1)
+    probs = torch.exp(log_probs)
+    entropy = -(probs * log_probs).sum(dim=-1)
+    return entropy
+
+
+def get_response_log_probs(
+    model: PreTrainedModel,
+    input_ids: torch.Tensor,
+    labels: torch.Tensor,
+    return_token_entropy: bool,
+) -> dict[str, torch.Tensor]:
+    device = torch.device(
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    model.to(device)
+    input_ids = input_ids.to(device)
+    labels = labels.to(device)
+
+    model.eval()
+    with torch.inference_mode():
+        outputs = model(input_ids)
+        logits = outputs.logits
+    log_probs = F.log_softmax(logits, dim=-1)  # B x SL x V
+    response_log_probs = log_probs.gather(-1, labels.unsqueeze(-1)).squeeze(
+        -1
+    )  # B x SL
+    ret = {"log_probs": response_log_probs}
+    if return_token_entropy:
+        token_entropy = compute_entropy(logits)  # B x SL
+        ret["token_entropy"] = token_entropy
+    return ret
